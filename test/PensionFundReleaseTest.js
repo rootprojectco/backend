1e1af28593a95b302cf40eb3539e724a6c4a92a1
alf40k <alf40k@gmail.com>
2017-07-21 00:27:53 -0400

    change promises to async/await

--- test/PensionFundReleaseTest.js
+++ test/PensionFundReleaseTest.js
@@ -13,88 +13,75 @@ contract('PensionFundRelease', (accounts) => {
 
   var deployParams = token => [validators, worker, 100, 1500243470, 604800, 100, token]
 
-  it("should return firstPaymentPercent", () =>
-    PensionFundRelease.new.apply(this, deployParams('0x0'))
-    .then(instance => instance.firstPaymentPercent.call())
-    .then(percent => percent.toNumber())
-    .should.eventually.equal(100)
-  )
-
-  it("should return validators", () =>
-    PensionFundRelease.new.apply(this, deployParams('0x0'))
-    .then(instance => instance.validators.call(0))
-    .should.eventually.equal(validators[0])
-  )
-  
-  it("should allow validators to vote", () => {
-    var instance
-    return PensionFundRelease.new.apply(this, deployParams('0x0'))
-    .then(inst => { instance = inst })
-    .then(() => instance.vote(true, "justification", {from: validators[0]}))
-    .then(() => instance.voteIndex.call(validators[0]))
-    .then(index => instance.votes.call(index))
-    .then(vote => vote[2])
-    .should.be.eventually.equal("justification")
+  it("should return firstPaymentPercent", async () => {
+    let instance = await PensionFundRelease.new.apply(this, deployParams('0x0'))
+    let percent = await instance.firstPaymentPercent.call()
+    let number = await percent.toNumber()
+    number.should.equal(100)
   })
 
-  it("should not allow non-validators to vote", () =>
+  it("should return validators", async () => {
+    let instance = await PensionFundRelease.new.apply(this, deployParams('0x0'))
+    let validator = await instance.validators.call(0)
+    validator.should.equal(validators[0])
+  })
+  
+  it("should allow validators to vote", async () => {
+    let instance = await PensionFundRelease.new.apply(this, deployParams('0x0'))
+    await instance.vote(true, "justification", {from: validators[0]})
+    let index = await instance.voteIndex.call(validators[0])
+    let vote = await instance.votes.call(index)
+    vote[2].should.be.equal("justification")
+  })
+
+  it("should not allow non-validators to vote", async () => {
      PensionFundRelease.new.apply(this, deployParams('0x0'))
      .then(inst => instance.vote(true, "justification", {from: unauthorized}))
      .should.be.rejected
-  )
-
-  it("should allow release after all validators approval", () => {
-    var instance
-    return PensionFundRelease.new.apply(this, deployParams('0x0'))
-    .then(inst => { instance = inst })
-    .then(() => instance.vote(true, "justification", {from: validators[0]}))
-    .then(() => instance.vote(true, "justification", {from: validators[1]}))
-    .then(() => instance.isReleaseApproved.call())
-    .should.be.eventually.equal(true)
   })
 
-  it("should not allow release if not all validators approved release", () => {
-    var instance
-    return PensionFundRelease.new.apply(this, deployParams('0x0'))
-    .then(inst => { instance = inst })
-    .then(() => instance.vote(true, "justification", {from: validators[0]}))
-    .then(() => instance.isReleaseApproved.call())
-    .should.be.eventually.equal(false)
+  it("should allow release after all validators approval", async () => {
+    let instance = await PensionFundRelease.new.apply(this, deployParams('0x0'))
+    await instance.vote(true, "justification", {from: validators[0]})
+    await instance.vote(true, "justification", {from: validators[1]})
+    let approval =  await instance.isReleaseApproved.call()
+    approval.should.be.equal(true)
   })
 
-  it("should allow burn after all validators rejection", () => {
-    var instance
-    return PensionFundRelease.new.apply(this, deployParams('0x0'))
-    .then(inst => { instance = inst})
-    .then(() => instance.vote(false, "justification", {from: validators[0]}))
-    .then(() => instance.vote(false, "justification", {from: validators[1]}))
-    .then(() => instance.isBurnApproved.call())
-    .should.be.eventually.equal(true)
+  it("should not allow release if not all validators approved release", async () => {
+    let instance = await PensionFundRelease.new.apply(this, deployParams('0x0'))
+    await instance.vote(true, "justification", {from: validators[0]})
+    let vote = await instance.isReleaseApproved.call()
+    vote.should.be.equal(false)
   })
 
-  it("should not allow burn if not all validators rejected release", () => {
-    var instance
-    return PensionFundRelease.new.apply(this, deployParams('0x0'))
-    .then(inst => { instance = inst })
-    .then(() => instance.vote(false, "justification", {from: validators[0]}))
-    .then(() => instance.isBurnApproved.call())
-    .should.be.eventually.equal(false)
+  it("should allow burn after all validators rejection", async () => {
+    let instance = await PensionFundRelease.new.apply(this, deployParams('0x0'))
+    await instance.vote(false, "justification", {from: validators[0]})
+    await instance.vote(false, "justification", {from: validators[1]})
+    let approval = await instance.isBurnApproved.call()
+    approval.should.be.equal(true)
   })
 
-  it("should release roots if all conditions met", () => {
-    var fund, token
-    var balance = 100
-    return Token.deployed()
-    .then(inst => { token = inst })
-    .then(() => PensionFundRelease.new.apply(this, deployParams(token.address)))
-    .then(inst => { fund = inst })
-    .then(() => token.transfer(fund.address, balance))
-    .then(() => fund.vote(true, "justification", {from: validators[0]}))
-    .then(() => fund.vote(true, "justification", {from: validators[1]}))
-    .then(() => fund.releaseRoots({from: worker}))
-    .then(() => fund.firtPaymentReleased.call().should.be.eventually.equal(true))
-    .then(() => token.balanceOf(worker))
-    .then(workerBalance => workerBalance.toNumber())
-    .should.be.eventually.equal(balance)
+  it("should not allow burn if not all validators rejected release", async () => {
+    let instance = await PensionFundRelease.new.apply(this, deployParams('0x0'))
+    await instance.vote(false, "justification", {from: validators[0]})
+    let approval = await instance.isBurnApproved.call()
+    approval.should.be.equal(false)
+  })
+
+  it("should release roots if all conditions met", async () => {
+    let fund, token, workerBalance, balanceNumber
+    let balance = 100
+    token = await Token.deployed()
+    fund = await PensionFundRelease.new.apply(this, deployParams(token.address))
+    await token.transfer(fund.address, balance)
+    await fund.vote(true, "justification", {from: validators[0]})
+    await fund.vote(true, "justification", {from: validators[1]})
+    await fund.releaseRoots({from: worker})
+    await fund.firtPaymentReleased.call().should.be.eventually.equal(true)
+    workerBalance = await token.balanceOf(worker)
+    balanceNumber = await workerBalance.toNumber()
+    balanceNumber.should.be.equal(balance)
   })
 })
