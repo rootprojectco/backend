f99ca5bec44a710681cee1674d15e8b0c39acc99
Alex Bazhanau <sayshayx@gmail.com>
2017-07-26 19:08:01 +0300

    IOU ROOTS token (#18)
    * IouRootsToken implemented
    
    * PensionFundRelease deployment
    
    * indention fixed
    
    * name, symbol and decimals added into IouRootsToken
    
    * contracts json added
    
    * comments added for IouRootsToken
    
    * token tests added
    
    * token tests cleared
    
    * token tests cleared
    
    * dev network port fixed
    
    * contracts json updated

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    live: {
+      host: "localhost",
+      port: 8545,
+      network_id: 1
     }
   }
 };
