68105c2da6f5c4d91ff21612d9a0772056c300ad
Alex Bazhanau <bazhanau@outlook.com>
2017-08-27 11:24:22 +0300

    indention fixed

--- test/IouRootsTokenTest.js
+++ test/IouRootsTokenTest.js
@@ -8,38 +8,38 @@ let IouRootsToken = artifacts.require("./IouRootsToken.sol")
 
 
 contract('IouRootsToken', (accounts) => {
-  let wallet = accounts[1]
-  let beneficiary = accounts[2]
-  let sender = accounts[3]
+    let wallet = accounts[1]
+    let beneficiary = accounts[2]
+    let sender = accounts[3]
 
-  const VALUE_TO_SEND = 10000
-  const RATE = 15000
-  const DECIMAL = 18
+    const VALUE_TO_SEND = 10000
+    const RATE = 15000
+    const DECIMAL = 18
 
 
-  let deployParams = [
-    RATE,
-    wallet,
-    'IOU ROOTS',
-    'IOR',
-    DECIMAL
-  ]
+    let deployParams = [
+        RATE,
+        wallet,
+        'IOU ROOTS',
+        'IOR',
+        DECIMAL
+    ]
 
-  it("should return firstPaymentPercent(fallback)", async () => {
-    let instance = await IouRootsToken.new.apply(this, deployParams)
-    let sendValue = await instance.sendTransaction({ from: sender, value: VALUE_TO_SEND })
-    let value = await instance.balanceOf(sender)
-    console.log("Sender:" + sender)
-    console.log("Balance: " + value)
-    value.toNumber().should.be.equal(VALUE_TO_SEND * RATE)
-  })
+    it("should return firstPaymentPercent(fallback)", async () => {
+        let instance = await IouRootsToken.new.apply(this, deployParams)
+        let sendValue = await instance.sendTransaction({ from: sender, value: VALUE_TO_SEND })
+        let value = await instance.balanceOf(sender)
+        console.log("Sender:" + sender)
+        console.log("Balance: " + value)
+        value.toNumber().should.be.equal(VALUE_TO_SEND * RATE)
+    })
 
-  it("should return firstPaymentPercent(non-fallback)", async () => {
-    let instance = await IouRootsToken.new.apply(this, deployParams)
-    let sendValue = await instance.buyTokens(beneficiary, { from: sender, value: VALUE_TO_SEND })
-    value = await instance.balanceOf(beneficiary)
-    console.log("Beneficiary:" + value)
-    value.toNumber().should.be.equal(VALUE_TO_SEND * RATE)
-  })
+    it("should return firstPaymentPercent(non-fallback)", async () => {
+        let instance = await IouRootsToken.new.apply(this, deployParams)
+        let sendValue = await instance.buyTokens(beneficiary, { from: sender, value: VALUE_TO_SEND })
+        value = await instance.balanceOf(beneficiary)
+        console.log("Beneficiary:" + value)
+        value.toNumber().should.be.equal(VALUE_TO_SEND * RATE)
+    })
 
 })
