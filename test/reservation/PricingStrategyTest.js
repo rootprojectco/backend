1147ed7184d3f7062539e2c1ca4a633d7cce2d11
Alex Bazhanau <bazhanau@outlook.com>
2017-09-25 21:32:19 +0300

    PricingStrategyTests fixed

--- test/reservation/PricingStrategyTest.js
+++ test/reservation/PricingStrategyTest.js
@@ -3,37 +3,37 @@ let BigNumber = require('bignumber.js');
 let PricingStrategy = artifacts.require("reservation/PricingStrategy.sol")
 
 contract('PricingStrategy', () => {
-    const NOW = web3.eth.getBlock(web3.eth.blockNumber).timestamp
-    const NEW_RATE_TIME = NOW + 100000
     const RATE1 = 11000
     const RATE2 = 12000
     const MINIMUM_WEI_AMOUNT = new BigNumber(web3.toWei(20, 'ether'))
 
-    let deployParams = [
-        NEW_RATE_TIME,
+    let deployParams = (newRateTime) => [
+        newRateTime,
         RATE1,
         RATE2,
         MINIMUM_WEI_AMOUNT,
     ]
 
     it("should return 0 if amount < MINIMUM_WEI_AMOUNT", async () => {
-        let instance = await PricingStrategy.new.apply(this, deployParams)
+        let now = web3.eth.getBlock(web3.eth.blockNumber).timestamp
+        let instance = await PricingStrategy.new.apply(this, deployParams(now + 10000))
         let amount = MINIMUM_WEI_AMOUNT.sub(1)
         let result = await instance.calculateTokenAmount(amount)
         assert(result.equals(0))
     })
 
     it("should return tokens by RATE1 if now < NEW_RATE_TIME", async () => {
-        let instance = await PricingStrategy.new.apply(this, deployParams)
+        let now = web3.eth.getBlock(web3.eth.blockNumber).timestamp
+        let instance = await PricingStrategy.new.apply(this, deployParams(now + 10000))
         let amount = MINIMUM_WEI_AMOUNT
         let result = await instance.calculateTokenAmount(amount)
-        console.log(result)
         assert(result.equals(amount.mul(RATE1)))
     })
 
     it("should return tokens by RATE2 if now >= NEW_RATE_TIME", async () => {
         let now = web3.eth.getBlock(web3.eth.blockNumber).timestamp
-        let delta = NEW_RATE_TIME - now + 100
+        let newRateTime = now + 10000
+        let delta = newRateTime - now + 100
         web3.currentProvider.send({
             jsonrpc: '2.0',
             method: 'evm_increaseTime',
@@ -41,7 +41,7 @@ contract('PricingStrategy', () => {
             id: new Date().getTime()
         })
 
-        let instance = await PricingStrategy.new.apply(this, deployParams)
+        let instance = await PricingStrategy.new.apply(this, deployParams(newRateTime))
         let amount = MINIMUM_WEI_AMOUNT
         let result = await instance.calculateTokenAmount(amount)
         assert(result.equals(amount.mul(RATE2)))
